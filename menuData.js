fd1a7dddf3474d0f2053ce1306e5df7183e7f72e
arzuakhik <arzu2050@gmail.com>
2021-02-14 13:36:38 +0600

    Initialize project using Create React App

---
+++ menuData.js
@@ -0,0 +1,133 @@
+export const menuData = [
+    {
+        name: "Home",
+        child: []
+    },
+    {
+        name: 'Institute Info',
+        child: [
+            {
+                name: 'Academic Info',
+                child: [
+                    {name: "WelcomeSpeech", child: []},
+                    {name: "Comittee", child: []},
+                    {name: "Donor Member", child: []},
+                ]
+            },
+            {
+                name: 'List Of',
+                child: [
+                    {name: "Teacher", child: []},
+                    {name: "Student", child: []},
+                    {name: "Staff", child: []},
+                ]
+            },
+            {
+                name: 'About',
+                child: [
+                    {name: "Institute Details", child: []},
+                ]
+            }
+        ]
+    },
+    {
+        name: "Academic Info",
+        child: [
+            {
+                name: "Details Info",
+                child: [
+                    {name: "Book List", child: []},
+                    {name: "Uniform", child: []},
+                    {name: "Fees Info", child: []},
+                    {name: "Seat Info", child: []},
+                    {name: "Syllabus", child: []},
+                ]
+            },
+            {
+                name: "Student Attendance",
+                child: [
+                    {name: "Institute Wise", child: []},
+                    {name: "Section Wise", child: []},
+                    {name: "Student Wise", child: []},
+                ]
+            },
+            {
+                name: "Fees Info",
+                child: [
+                    {name: "Find Due Info", child: []},
+                ]
+            },
+            {
+                name: "Routine Info",
+                child: [
+                    {name: "Class Routine", child: []},
+                    {name: "Online Schedule", child: []},
+                    {name: "Exam Routine", child: []},
+                ]
+            },
+        ]
+    },
+    {
+        name: "Result Info",
+        child: [
+            {
+                name: "Semester Exam",
+                child: [
+                    {name: "Merit List", child: []},
+                    {name: "Fail List", child: []},
+                    {name: "Section Wise", child: []},
+                    {name: "Individual", child: []},
+                ]
+            },
+            {
+                name: "Public Exam",
+                child: [
+                    {name: "Success Student", child: []},
+                ]
+            },
+        ]
+    },
+    {
+        name: "More",
+        child: [
+            {
+                name: "Gallery",
+                child: [
+                    {name: "Photo Gallery", child: []},
+                    {name: "Video Gallery", child: []},
+                    {name: "Award Gallery", child: []},
+                    {name: "Top Student", child: []},
+                    {name: "Success Alumns", child: []},
+                ],
+            },
+            {
+                name: "More",
+                child: [
+                    {name: "Academic Calender", child: []},
+                    {name: "Institute Notice", child: []},
+                    {name: "Important Link", child: []},
+                    {name: "Download Corner", child: []},
+                    {name: "Job Circular", child: []},
+                    {name: "Contact", child: []},
+                ],
+            },
+            {
+                name: "Login",
+                child: [
+                    {name: "Student", child: []},
+                    {name: "Guardian", child: []},
+                    {name: "Teacher", child: []},
+                ],
+            },
+            {
+                name: "Online Admission",
+                child: [
+                    {name: "Notice Admission", child: []},
+                    {name: "Application", child: []},
+                    {name: "Find Result", child: []},
+                    {name: "Admit Card", child: []},
+                ],
+            },
+        ]
+    }
+]
